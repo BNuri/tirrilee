4c567312c0c58a90626775b6758abab5826cceee
BNURI <luckmon05@naver.com>
2020-06-02 06:14:53 +0900

    update: Text

--- src/Components/Input/Text.js
+++ src/Components/Input/Text.js
@@ -4,8 +4,8 @@ import styled from "styled-components";
 const Input = styled.input`
   height: 22pt;
   width: 100%;
-  padding: 0px 6pt;
-  margin: 2px 0px;
+  padding: 0 6pt;
+  margin: 2pt 0;
   border: 0.5pt solid ${(props) => props.theme.border_lightgrey};
   border-radius: 2pt;
   color: ${(props) => props.theme.main_lightgrey};
