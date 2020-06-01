5cbffc63c1fada3b13b2100eded4e1bba16e9249
BNURI <luckmon05@naver.com>
2020-06-02 04:01:57 +0900

    update: Theme text brown

--- src/Theme.js
+++ src/Theme.js
@@ -4,6 +4,7 @@ const theme = {
   text_grey: "rgb(118, 118, 118)",
   text_mediumgrey: "rgb(112, 112, 112)",
   text_lightgrey: "rgb(198, 198, 198)",
+  text_brown: "rgb(56 31 31)",
   border_lightgrey: "rgb(230, 230, 230)",
   button_lightgrey: "rgb(245, 245, 245)",
 };
