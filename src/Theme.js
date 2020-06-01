94a08249708d418e07f0482baf651cd9f5d02e0d
BNURI <luckmon05@naver.com>
2020-06-01 11:59:06 +0900

    update: Theme

--- src/Theme.js
+++ src/Theme.js
@@ -2,9 +2,10 @@ const theme = {
   main_blue: "#0379c7",
   main_white: "#ffffff",
   text_grey: "rgb(118, 118, 118)",
-  text_midiumgrey: "rgb(112, 112, 112)",
+  text_mediumgrey: "rgb(112, 112, 112)",
   text_lightgrey: "rgb(198, 198, 198)",
   border_lightgrey: "rgb(230, 230, 230)",
+  button_lightgrey: "rgb(245, 245, 245)",
 };
 
 export default theme;
