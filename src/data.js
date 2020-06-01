a0e0a84fbc32f3b8780b9f0015edf509ddd6b901
BNURI <luckmon05@naver.com>
2020-06-01 12:25:17 +0900

    add: data

---
+++ src/data.js
@@ -0,0 +1,78 @@
+export const items = [
+  {
+    name: "친환경 딸기",
+    price: 10000,
+    season: "봄",
+    imgPath:
+      "https://post-phinf.pstatic.net/MjAyMDAyMTlfMTcg/MDAxNTgyMDcyODM1MTI5.Le7ni6zywFjwfNELHYHuF1GHw8UgWBFsCK41EVKck7Mg.B17598o_kp3qB8SX_P_Mzfd44Pup6wA7_q2sMsbyddMg.PNG/1485843010TITLE_IMG_.png?type=w1200",
+    rating: 4.9,
+    seller: "이티릴",
+    id: "1",
+  },
+  {
+    name: "고당도 수박",
+    price: 12000,
+    season: "여름",
+    imgPath:
+      "https://s3.ap-northeast-2.amazonaws.com/img.kormedi.com/news/article/__icsFiles/artimage/2018/07/05/c_km601/shutterstock_583745164_580.jpg",
+    rating: 4.9,
+    seller: "이티릴",
+    id: "2",
+  },
+  {
+    name: "아오리 사과",
+    price: 10000,
+    season: "가을",
+    imgPath:
+      "https://img1.tmon.kr/cdn3/deals/2019/08/12/2331486054/original_2331486054_front_dc858_1565597487production.jpg",
+    rating: 4.9,
+    seller: "이티릴",
+    id: "3",
+  },
+  {
+    name: "제주감귤",
+    price: 12000,
+    season: "겨울",
+    imgPath:
+      "https://img.danawa.com/prod_img/500000/799/560/img/6560799_1.jpg?shrink=500:500&_v=20181101105718",
+    rating: 4.9,
+    seller: "이티릴",
+    id: "4",
+  },
+  {
+    name: "고당도 딸기",
+    price: 20000,
+    season: "봄",
+    imgPath:
+      "https://img.kr.news.samsung.com/kr/wp-content/uploads/2017/02/%ED%91%B8%EB%93%9C%ED%8F%AC%EC%BB%A4%EC%8A%A42%ED%8E%B807.jpg",
+    rating: 4.9,
+    seller: "이티릴",
+    id: "5",
+  },
+];
+
+export const user = {
+  name: "이티릴",
+  email: "tirrilee@gmail.com",
+  description: "안녕하세요. 이티릴입니다. 소개내용이 들어갑니다.",
+  profilePath: "",
+  sellCnt: 100,
+  buyCnt: 200,
+  season: "봄",
+};
+
+export const article1 = {
+  title: "티릴리 과일농장",
+  backImg:
+    "https://lh3.googleusercontent.com/proxy/a6b3cVlSBua6N2xXBLIUmmAzTBe7lPzqyb1rZL-4Egwc1YtYEKlLEh-cuxZ5DZLybAmtyUcYPrxeSWOndvgbqyPiC3HUMyanspaEAjANZ3930rBRUQz0-qd31wHR9FSPZgTe7ruEuuOS4A",
+  link: "/main",
+  buttonTitle: "구경가기",
+};
+
+export const article2 = {
+  title: "이번 주 베스트 과일후기",
+  backImg:
+    "https://post-phinf.pstatic.net/MjAxOTAxMDlfOTUg/MDAxNTQ3MDEyNDEzMzMy.9Himg8nE2ptT4KVeoIn412n5IDArNQbZxhcjwaHoCI4g.hKRMQiet3fm8d-bKn0LtsNTYJTxiItud2CqT-D-EHqIg.JPEG/%EC%82%AC%EA%B3%BC_3.jpg?type=w1200",
+  link: "/main",
+  buttonTitle: "보러가기",
+};
