eaef268d52a861e8e71d928116928a19ceef29ec
BNURI <luckmon05@naver.com>
2020-06-02 05:29:35 +0900

    update: EditMyPage

---
+++ src/Components/Nav/HeaderWithComplete.js
@@ -0,0 +1,33 @@
+import React from "react";
+import styled from "styled-components";
+import { withRouter } from "react-router-dom";
+import back_button_grey from "../../assets/images/icons/back_button_grey.png";
+
+const Header = styled.header`
+  height: 22pt;
+  width: 100%;
+  padding: 0 6pt;
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Img = styled.img`
+  height: 12pt;
+  width: 12pt;
+`;
+
+const Complete = styled.span`
+  font-size: 7pt;
+  color: ${(props) => props.theme.main_blue};
+`;
+
+export default withRouter(({ history, handleSubmit }) => (
+  <Header>
+    <Img src={back_button_grey} alt="뒤로가기" onClick={history.goBack} />
+    <Complete onClick={handleSubmit}>완료</Complete>
+  </Header>
+));
--- src/Screens/EditMyPage.js
+++ src/Screens/EditMyPage.js
@@ -1,6 +1,139 @@
-import React from "react";
-import styled from "Styled-components";
+import React, { useState } from "react";
+import styled from "styled-components";
+import Header from "../Components/Nav/HeaderWithComplete";
+import Navigation from "../Components/Nav/Navigation";
+import { user } from "../data";
 
 const Container = styled.div``;
 
-export default () => <Container>Edit</Container>;
+const Main = styled.main``;
+
+const ImageContainer = styled.div`
+  height: 82pt;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+  border-bottom: 0.3pt solid ${(props) => props.theme.button_lightgrey};
+`;
+
+const Image = styled.div`
+  height: 40pt;
+  width: 40pt;
+  border-radius: 50%;
+  background-image: url(${(props) => props.imgPath});
+  background-size: cover;
+  background-position: center;
+`;
+
+const FileInput = styled.input`
+  display: none;
+`;
+
+const UploadButton = styled.button`
+  height: 16pt;
+  width: 50pt;
+  border-radius: 4pt;
+  font-size: 6pt;
+  color: ${(props) => props.theme.main_blue};
+  background-color: ${(props) => props.theme.button_lightgrey};
+  border: none;
+`;
+
+const InputContainer = styled.div`
+  margin: 16pt 6pt 0pt 6pt;
+  display: flex;
+  align-items: center;
+`;
+
+const Label = styled.label`
+  display: inline-block;
+  width: 37pt;
+  font-size: 7pt;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  padding: 4pt 2pt;
+  outline: 0;
+  border-width: 0 0 1pt;
+  border-color: ${(props) => props.theme.button_lightgrey};
+  color: ${(props) => props.theme.text_grey};
+`;
+
+export default () => {
+  const [form, setForm] = useState(user);
+  const updateValue = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const uploadFile = () => {
+    const input = document.querySelector("#fileUpload");
+    input.click();
+  };
+  const updateFile = (e) => {
+    e.preventDefault();
+    const reader = new FileReader();
+    const file = e.target.files[0];
+    reader.onloadend = () => {
+      setForm({
+        ...form,
+        file: file,
+        profilePath: reader.result,
+      });
+    };
+    reader.readAsDataURL(file);
+  };
+  const handleSubmit = () => {};
+
+  return (
+    <Container>
+      <Header handleSubmit={handleSubmit} />
+      <Main className="noPadding withHeaderNav">
+        <ImageContainer>
+          <Image imgPath={form.profilePath} />
+          <FileInput
+            id="fileUpload"
+            type="file"
+            accept="image/jpg,image/png,image/jpeg"
+            onChange={updateFile}
+          />
+          <UploadButton onClick={uploadFile}>프로필 변경</UploadButton>
+        </ImageContainer>
+        <InputContainer>
+          <Label htmlFor="name">이름</Label>
+          <Input
+            type="text"
+            defaultValue={form.name}
+            name="name"
+            onChange={updateValue}
+            id="name"
+          />
+        </InputContainer>
+        <InputContainer>
+          <Label htmlFor="email">이메일</Label>
+          <Input
+            type="text"
+            defaultValue={form.email}
+            name="email"
+            onChange={updateValue}
+            id="email"
+          />
+        </InputContainer>
+        <InputContainer>
+          <Label htmlFor="description">소개</Label>
+          <Input
+            type="text"
+            defaultValue={form.description}
+            name="description"
+            onChange={updateValue}
+            id="description"
+          />
+        </InputContainer>
+      </Main>
+      <Navigation />
+    </Container>
+  );
+};
